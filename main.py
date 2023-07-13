a4dc9d8888b4de86318a46c3c2c7b81a3fe643fd
Keith Petersen <keith.petersen@rgare.com>
2023-07-13 15:06:34 -0500

    MRE of logging_config wrapper_class bug

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 """Minimal Starlite application."""
 from typing import Any
 
-from starlite import Starlite, get
+import logging
+import structlog
+from starlite import Starlite, get, StructLoggingConfig
+
+main_logger = structlog.getLogger("main")
 
 
 @get("/")
 def hello_world() -> dict[str, Any]:
     """Route Handler that outputs hello world."""
+    main_logger.info("hello world endpoint called INFO")
+    main_logger.debug("hello world endpoint called DEBUG")
     return {"hello": "world"}
 
 
-app = Starlite(route_handlers=[hello_world])
+logging_config = StructLoggingConfig(
+    wrapper_class=structlog.make_filtering_bound_logger(logging.DEBUG)
+)
+
+
+app = Starlite(route_handlers=[hello_world], logging_config=logging_config)
